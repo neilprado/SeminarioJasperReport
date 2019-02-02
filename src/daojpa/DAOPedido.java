77a34de5eb8382509d0f3d69bf0ce6fc622080aa
neilprado <neiljohn17@gmail.com>
2019-02-02 11:16:30 -0300

    Queries in DAO

--- src/daojpa/DAOPedido.java
+++ src/daojpa/DAOPedido.java
@@ -1,15 +1,30 @@
 package daojpa;
 
+import java.util.List;
+
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 
+import modelo.Cliente;
 import modelo.Pedido;
+import modelo.Produto;
 
 public class DAOPedido extends DAO<Pedido> {
-	public Pedido buscarPedido(int id) {
+	public Pedido consultarPedidoCliente(Cliente c) {
 		try {
-			Query q = manager.createQuery("select p from Pedido p where p.id='" + id + "'");
+			Query q = manager.createQuery("select p from Pedido p where p.cliente='" + c + "'");
 			return (Pedido) q.getSingleResult();
+		}catch(NoResultException e) {
+			return null;
+		}
+	}
+	
+	public List<Pedido> listarPedidosComProduto(Produto p){
+		try {
+			Query q = manager.createQuery("select p from Pedido p join pr.pedido prod where prod.produto='" + p + "'");
+			@SuppressWarnings("unchecked")
+			List<Pedido> resultado = q.getResultList();
+			return resultado;
 		}catch(NoResultException e) {
 			return null;
 		}
